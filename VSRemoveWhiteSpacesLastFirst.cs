8a8ed8e7c186057437c6f48a9359e13969db0b54
William Collins <williamc932@gmail.com>
2012-05-24 10:47:00 -0500

    initial commit

---
+++ VSRemoveWhiteSpacesLastFirst.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Text;
+using System.Collections.Generic;
+using Emc.InputAccel.IndexPlus.Scripting;
+using Emc.InputAccel.QuickModule.ClientScriptingInterface;
+
+namespace VSRemoveWhiteSpacesLastFirst
+{
+    public class FieldEvents
+        : IFieldEvents
+    {
+        public void Initialize(IFieldEventInformation info)
+        {
+        }
+
+        public void Changed(IFieldEventInformation info)
+        {
+            if (info.Sender.Id == "Last Name")
+            {
+                IIndexFields nodeFields = info.GetCurrentNodeFields();
+                IIndexField lastName = nodeFields.GetFieldById("Last Name");
+                if (lastName != null)
+                {
+                    lastName.Value = lastName.Value.Trim();
+                }
+            }
+            if (info.Sender.Id == "First Name")
+            {
+                IIndexFields nodeFields = info.GetCurrentNodeFields();
+                IIndexField firstName = nodeFields.GetFieldById("First Name");
+                if (firstName != null)
+                {
+                    firstName.Value = firstName.Value.Trim();
+                }
+            }
+        }
+
+        public void Populate(IFieldEventInformation info)
+        {
+        }
+
+        public ValidationResult Validate(IFieldEventInformation info, ValidationReason validationReason, Boolean silent)
+        {
+            return default(ValidationResult);
+        }
+    }
+}
